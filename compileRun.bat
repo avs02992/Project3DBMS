ba61e55f3b218816bb63ca7406c343483a3bec44
jokzcooks <jokzcooks@gmail.com>
2024-04-14 18:14:59 -0400

    file structure

---
+++ compileRun.bat
@@ -0,0 +1,11 @@
+echo Compiling class files...
+@REM EXAMPLE COMPILE
+@REM javac src/ArrayUtil.java -d bin
+@REM javac src/KeyType.java -d bin
+@REM javac src/Table.java -d bin -cp bin
+@REM javac src/MovieDB.java -d bin -cp bin
+
+echo Running driver...
+@REM EXAMPLE RUN
+@REM java -cp bin src.MovieDB
+@REM java -cp bin src.KeyType
